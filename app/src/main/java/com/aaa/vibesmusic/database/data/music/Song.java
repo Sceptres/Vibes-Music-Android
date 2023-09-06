773699984108cdbfe460d722bd2d5c8f1405c9cd
Abdallah Alqashqish <alqashqa@mcmaster.ca>
2023-09-06 14:52:17 -0400

    Implement song equality and hash code

--- app/src/main/java/com/aaa/vibesmusic/database/data/music/Song.java
+++ app/src/main/java/com/aaa/vibesmusic/database/data/music/Song.java
@@ -4,6 +4,8 @@ import androidx.room.ColumnInfo;
 import androidx.room.Entity;
 import androidx.room.PrimaryKey;
 
+import java.util.Objects;
+
 @Entity(tableName = "Songs")
 public class Song {
     @PrimaryKey
@@ -11,4 +13,17 @@ public class Song {
 
     @ColumnInfo(name = "location")
     public String location;
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (o == null || getClass() != o.getClass()) return false;
+        Song song = (Song) o;
+        return id == song.id && Objects.equals(location, song.location);
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hash(id, location);
+    }
 }
