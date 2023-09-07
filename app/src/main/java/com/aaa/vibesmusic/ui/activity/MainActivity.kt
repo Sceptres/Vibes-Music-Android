3e841f3c55c35f4c3350e387013c482af88711fe
Abdallah Alqashqish <alqashqa@mcmaster.ca>
2023-09-07 18:58:42 -0400

    Add and implement fragment container view in main activity

--- app/src/main/java/com/aaa/vibesmusic/ui/activity/MainActivity.kt
+++ app/src/main/java/com/aaa/vibesmusic/ui/activity/MainActivity.kt
@@ -2,8 +2,17 @@ package com.aaa.vibesmusic.ui.activity
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.util.Log
+import android.view.MenuItem
+import androidx.navigation.NavController
+import androidx.navigation.findNavController
+import androidx.navigation.fragment.NavHostFragment
+import androidx.navigation.ui.NavigationUI
+import androidx.navigation.ui.onNavDestinationSelected
+import androidx.navigation.ui.setupWithNavController
 import com.aaa.vibesmusic.R
 import com.aaa.vibesmusic.databinding.ActivityMainBinding
+import com.google.android.material.bottomnavigation.BottomNavigationView
 
 class MainActivity : AppCompatActivity() {
     private var _binding: ActivityMainBinding? = null
@@ -12,8 +21,16 @@ class MainActivity : AppCompatActivity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
 
         _binding = ActivityMainBinding.inflate(this.layoutInflater)
+
+        setContentView(binding.root)
+
+        val bottomNav: BottomNavigationView = binding.bottomNav
+        val navHostFragment =
+            supportFragmentManager.findFragmentById(R.id.nav_host_fragment) as NavHostFragment
+        val navController = navHostFragment.navController
+
+        bottomNav.setupWithNavController(navController)
     }
 }
