2846da5de44c85d29daa6e4b29805c956f8d5150
Abdallah Alqashqish <alqashqa@mcmaster.ca>
2023-09-06 21:55:32 -0400

    Fields of song are final and constructor used to instantiate

--- app/src/main/java/com/aaa/vibesmusic/database/data/music/Song.java
+++ app/src/main/java/com/aaa/vibesmusic/database/data/music/Song.java
@@ -2,6 +2,7 @@ package com.aaa.vibesmusic.database.data.music;
 
 import androidx.room.ColumnInfo;
 import androidx.room.Entity;
+import androidx.room.Ignore;
 import androidx.room.Index;
 import androidx.room.PrimaryKey;
 
@@ -10,10 +11,36 @@ import java.util.Objects;
 @Entity(tableName = "Songs", indices = {@Index(value = "location", unique = true)})
 public class Song {
     @PrimaryKey(autoGenerate = true)
-    public int id;
+    private final int id;
 
     @ColumnInfo(name = "location")
-    public String location;
+    private final String location;
+
+    public Song(int id, String location) {
+        this.id = id;
+        this.location = location;
+    }
+
+    @Ignore
+    public Song(String location) {
+        this(0, location);
+    }
+
+    /**
+     *
+     * @return The id of the song in the database
+     */
+    public int getId() {
+        return this.id;
+    }
+
+    /**
+     *
+     * @return The location of the song on the disk
+     */
+    public String getLocation() {
+        return this.location;
+    }
 
     @Override
     public boolean equals(Object o) {
