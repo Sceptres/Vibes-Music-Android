58bb5251d9f9cf72b1899db395effac5ad992a56
Abdallah Alqashqish <alqashqa@mcmaster.ca>
2023-09-08 09:34:38 -0400

    Clean imports

--- app/src/main/java/com/aaa/vibesmusic/ui/activity/MainActivity.kt
+++ app/src/main/java/com/aaa/vibesmusic/ui/activity/MainActivity.kt
@@ -1,14 +1,8 @@
 package com.aaa.vibesmusic.ui.activity
 
-import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
-import android.util.Log
-import android.view.MenuItem
-import androidx.navigation.NavController
-import androidx.navigation.findNavController
+import androidx.appcompat.app.AppCompatActivity
 import androidx.navigation.fragment.NavHostFragment
-import androidx.navigation.ui.NavigationUI
-import androidx.navigation.ui.onNavDestinationSelected
 import androidx.navigation.ui.setupWithNavController
 import com.aaa.vibesmusic.R
 import com.aaa.vibesmusic.databinding.ActivityMainBinding
