e1ba10e030399c03d5e5459b0d41c1dc2d9ee539
Abdallah Alqashqish <alqashqa@mcmaster.ca>
2023-10-22 01:30:02 -0400

    Move all importing of music into fragment All the importing of the songs and file operations are done in the fragment instead of the activity result contract

--- app/src/main/java/com/aaa/vibesmusic/ui/fragment/ImportSongsFragment.kt
+++ app/src/main/java/com/aaa/vibesmusic/ui/fragment/ImportSongsFragment.kt
@@ -1,22 +1,34 @@
 package com.aaa.vibesmusic.ui.fragment
 
+import android.net.Uri
 import android.os.Bundle
+import android.util.Log
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import androidx.activity.result.ActivityResultLauncher
 import androidx.fragment.app.Fragment
+import com.aaa.vibesmusic.R
 import com.aaa.vibesmusic.database.VibesMusicDatabase
+import com.aaa.vibesmusic.database.data.music.Song
+import com.aaa.vibesmusic.database.data.music.SongDao
 import com.aaa.vibesmusic.databinding.FragmentImportSongsBinding
+import com.aaa.vibesmusic.exceptions.SongAlreadyExistsException
+import com.aaa.vibesmusic.metadata.SongMetaData
+import com.aaa.vibesmusic.metadata.retriever.SongMetadataRetriever
+import com.aaa.vibesmusic.storage.StorageUtil
 import com.aaa.vibesmusic.ui.UIUtil
 import com.aaa.vibesmusic.ui.fragment.result.ImportSongsActivityResultContract
 import io.reactivex.android.schedulers.AndroidSchedulers
 import io.reactivex.disposables.CompositeDisposable
 import io.reactivex.schedulers.Schedulers
+import java.io.IOException
+import java.util.Objects
 
 class ImportSongsFragment : Fragment() {
     private var importSongsLauncher: ActivityResultLauncher<Void>? = null
     private val mDisposable: CompositeDisposable = CompositeDisposable()
+    private var db: VibesMusicDatabase? = null
 
     private var _binding: FragmentImportSongsBinding? = null
     private val binding: FragmentImportSongsBinding
@@ -24,25 +36,29 @@ class ImportSongsFragment : Fragment() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        this.importSongsLauncher = registerForActivityResult(
-            ImportSongsActivityResultContract(
-                this.requireActivity()
-            )
-        ) {
-                if(it.size > 0) {
-                    val database: VibesMusicDatabase = VibesMusicDatabase.getInstance(context)
+        this.db = VibesMusicDatabase.getInstance(context)
+
+        // Setup the import songs activity result launcher
+        this.importSongsLauncher = registerForActivityResult(ImportSongsActivityResultContract()) {
+            if(it.isNotEmpty()) {
+                val importedSongs: List<Song> = this.importSongsFromUris(it)
+
+                // Were there any songs imported to be added to the database?
+                if (importedSongs.isNotEmpty()) {
                     mDisposable.add(
-                        database.songDao().insertSongs(it)
+                        this.db!!.songDao().insertSongs(importedSongs)
                             .subscribeOn(Schedulers.io())
                             .observeOn(AndroidSchedulers.mainThread())
                             .subscribe {
-                                UIUtil.showLongToast(
-                                    requireContext(),
-                                    "Music imported successfully"
+                                UIUtil.showLongSnackBar(
+                                    requireView(),
+                                    "Music imported successfully",
+                                    resources.getColor(R.color.foreground_color, null)
                                 )
                             }
                     )
                 }
+            }
         }
     }
 
@@ -62,5 +78,98 @@ class ImportSongsFragment : Fragment() {
     override fun onStop() {
         super.onStop()
         this.mDisposable.clear()
+    }
+
+    /**
+     *
+     * @param contentUri The [Uri] of the song to extract
+     * @return The [Song] instance that has been imported
+     * @throws IOException If there was an error copying the song
+     * @throws SongAlreadyExistsException If the song being imported already exists
+     */
+    @Throws(IOException::class, SongAlreadyExistsException::class)
+    private fun parseUri(contentUri: Uri, metaData: SongMetaData): Song {
+        val name = metaData.name
+        val artist = metaData.artist
+        val albumName = metaData.albumName
+        val image = metaData.image
+
+        val songDao: SongDao = this.db!!.songDao()
+        if (songDao.doesSongExist(name, artist, albumName)) {
+            Log.d("SONG NAME", name)
+            throw SongAlreadyExistsException()
+        }
+
+        var songLocation: String?
+        var songImageLocation: String? = null
+        var wasSongSaved: Boolean
+
+        val resolver = this.requireActivity().contentResolver
+        resolver.openFileDescriptor(contentUri, "r").use { fd ->
+            val extension = StorageUtil.getExtension(resolver, contentUri)
+            val fileName = "$name.$extension"
+            wasSongSaved = StorageUtil.saveSong(fd!!.fileDescriptor, fileName)
+            songLocation = StorageUtil.getSongPath(fileName)
+        }
+
+        if (!wasSongSaved)
+            throw IOException("Unable to save the song!")
+
+        if (Objects.nonNull(image)) {
+            StorageUtil.saveSongImage(image, name)
+            songImageLocation = StorageUtil.getSongImagePath(name)
+        }
+        return Song(
+            songLocation!!,
+            name,
+            artist,
+            albumName,
+            songImageLocation
+        )
+    }
+
+    /**
+     *
+     * @param uris The [List] of [Uri]s that have been chosen by the user
+     * @return The [List] of [Song]s that were imported to the application
+     */
+    private fun importSongsFromUris(uris: List<Uri>): List<Song> {
+        val songs: MutableList<Song> = ArrayList()
+        val context = this.requireActivity().applicationContext
+
+        var currentSong = ""
+        val failedSongs: MutableList<String> = ArrayList()
+        val existingSongs: MutableList<String> = ArrayList()
+
+        for (uri in uris) {
+            try {
+                SongMetadataRetriever(context, uri).use { retriever ->
+                    val metaData = retriever.allMetaData
+                    currentSong = metaData.name
+                    val song = this.parseUri(uri, metaData)
+                    songs.add(song)
+                }
+            } catch (e: IOException) {
+                failedSongs.add(currentSong)
+            } catch (e: SongAlreadyExistsException) {
+                existingSongs.add(currentSong)
+            }
+        }
+
+        if (failedSongs.isNotEmpty())
+            UIUtil.showLongSnackBar(
+                this.requireView(),
+                "Unable to import the songs ${failedSongs.joinToString(", ")}! Please try again!",
+                resources.getColor(R.color.foreground_color, null)
+            )
+
+        if (existingSongs.isNotEmpty())
+            UIUtil.showLongSnackBar(
+                this.requireView(),
+                "The songs ${existingSongs.joinToString(", ")} have already been imported!",
+                resources.getColor(R.color.foreground_color, null)
+            )
+
+        return songs
     }
 }
--- app/src/main/java/com/aaa/vibesmusic/ui/fragment/result/ImportSongsActivityResultContract.java
+++ app/src/main/java/com/aaa/vibesmusic/ui/fragment/result/ImportSongsActivityResultContract.java
@@ -2,45 +2,19 @@ package com.aaa.vibesmusic.ui.fragment.result;
 
 import android.app.Activity;
 import android.content.ClipData;
-import android.content.ContentResolver;
 import android.content.Context;
 import android.content.Intent;
-import android.graphics.Bitmap;
 import android.net.Uri;
-import android.os.ParcelFileDescriptor;
 
 import androidx.activity.result.contract.ActivityResultContract;
 import androidx.annotation.NonNull;
 import androidx.annotation.Nullable;
 
-import com.aaa.vibesmusic.database.VibesMusicDatabase;
-import com.aaa.vibesmusic.database.data.music.Song;
-import com.aaa.vibesmusic.database.data.music.SongDao;
-import com.aaa.vibesmusic.exceptions.SongAlreadyExistsException;
-import com.aaa.vibesmusic.metadata.SongMetaData;
-import com.aaa.vibesmusic.metadata.retriever.SongMetadataRetriever;
-import com.aaa.vibesmusic.storage.StorageUtil;
-import com.aaa.vibesmusic.ui.UIUtil;
-
-import java.io.IOException;
-import java.lang.ref.WeakReference;
 import java.util.ArrayList;
 import java.util.List;
 import java.util.Objects;
 
-public class ImportSongsActivityResultContract extends ActivityResultContract<Void, List<Song>> {
-    private final WeakReference<Activity> activity;
-    private final VibesMusicDatabase db;
-
-    public ImportSongsActivityResultContract(@NonNull Activity activity) {
-        this.activity = new WeakReference<>(activity);
-        this.db = VibesMusicDatabase.getInstance(this.getActivity().getApplicationContext());
-    }
-
-    private Activity getActivity() {
-        return this.activity.get();
-    }
-
+public class ImportSongsActivityResultContract extends ActivityResultContract<Void, List<Uri>> {
     @NonNull
     @Override
     public Intent createIntent(@NonNull Context context, Void unused) {
@@ -52,9 +26,9 @@ public class ImportSongsActivityResultContract extends ActivityResultContract<Vo
     }
 
     @Override
-    public List<Song> parseResult(int i, @Nullable Intent intent) {
+    public List<Uri> parseResult(int i, @Nullable Intent intent) {
         if(i == Activity.RESULT_OK) {
-            List<Song> importedSongs = null;
+            List<Uri> importedSongs = null;
 
             if(Objects.nonNull(intent)) {
                 ClipData clipData = intent.getClipData();
@@ -75,119 +49,25 @@ public class ImportSongsActivityResultContract extends ActivityResultContract<Vo
     /**
      *
      * @param data The {@link ClipData} with all the {@link Uri}s to import
-     * @return The {@link List} of imported {@link Song}s
+     * @return The {@link List} of chosen {@link Uri}s
      */
-    private List<Song> parseClipData(ClipData data) {
-        List<Song> songs = new ArrayList<>();
-        Context context = this.getActivity().getApplicationContext();
-
-        String currentSong = "";
-        List<String> failedSongs = new ArrayList<>();
-        List<String> existingSongs = new ArrayList<>();
+    private List<Uri> parseClipData(ClipData data) {
+        List<Uri> uris = new ArrayList<>();
 
         for (int i = 0; i < data.getItemCount(); i++) {
             Uri uri = data.getItemAt(i).getUri();
-            try(SongMetadataRetriever retriever = new SongMetadataRetriever(context, uri)) {
-                SongMetaData metaData = retriever.getAllMetaData();
-                currentSong = metaData.getName();
-                Song song = this.parseUri(uri, metaData);
-                songs.add(song);
-            } catch (IOException e) {
-                failedSongs.add(currentSong);
-            } catch (SongAlreadyExistsException e) {
-                existingSongs.add(currentSong);
-            }
+            uris.add(uri);
         }
 
-        if(!failedSongs.isEmpty())
-            UIUtil.showLongToast(
-                    this.getActivity(),
-                    String.format("Unable to import the songs %s! Please try again!", String.join(", ", failedSongs))
-            );
-
-        if(!existingSongs.isEmpty())
-            UIUtil.showLongToast(
-                    this.getActivity(),
-                    String.format("The songs %s have already been imported!", String.join(", ", existingSongs))
-            );
-
-        return songs;
+        return uris;
     }
 
     /**
      *
      * @param data The {@link Uri} of the file to user chose
-     * @return The {@link List} of {@link Song}s imported by the user
+     * @return The {@link List} of chosen {@link Uri}s
      */
-    private List<Song> parseUriData(Uri data) {
-        List<Song> songs = new ArrayList<>();
-        Context context = this.getActivity().getApplicationContext();
-
-        String songName = "";
-
-        try(SongMetadataRetriever retriever = new SongMetadataRetriever(context, data)) {
-            SongMetaData metaData = retriever.getAllMetaData();
-            songName = metaData.getName();
-            Song song = this.parseUri(data, metaData);
-            songs.add(song);
-        } catch (IOException e) {
-            UIUtil.showLongToast(
-                    this.getActivity().getApplicationContext(),
-                    String.format("Unable to import the song %s. Please try again!", songName)
-            );
-        } catch (SongAlreadyExistsException e) {
-            UIUtil.showLongToast(
-                    this.getActivity(),
-                    String.format("The song %s has already been imported!", songName)
-            );
-        }
-        return songs;
-    }
-
-    /**
-     *
-     * @param contentUri The {@link Uri} of the song to extract
-     * @return The {@link Song} instance that has been imported
-     * @throws IOException If there was an error copying the song
-     * @throws SongAlreadyExistsException If the song being imported already exists
-     */
-    private Song parseUri(Uri contentUri, SongMetaData metaData) throws IOException, SongAlreadyExistsException {
-        String name = metaData.getName();
-        String artist = metaData.getArtist();
-        String albumName = metaData.getAlbumName();
-        Bitmap image = metaData.getImage();
-
-        SongDao songDao = this.db.songDao();
-        if(songDao.doesSongExist(name, artist, albumName))
-            throw new SongAlreadyExistsException();
-
-        String songLocation;
-        String songImageLocation = null;
-
-        boolean wasSongSaved;
-
-        ContentResolver resolver = this.getActivity().getContentResolver();
-        try(ParcelFileDescriptor fd = resolver.openFileDescriptor(contentUri, "r")) {
-            String extension = StorageUtil.getExtension(resolver, contentUri);
-            String fileName = name + "." + extension;
-            wasSongSaved = StorageUtil.saveSong(fd.getFileDescriptor(), fileName);
-            songLocation = StorageUtil.getSongPath(fileName);
-        }
-
-        if(!wasSongSaved)
-            throw new IOException("Unable to save the song!");
-
-        if(Objects.nonNull(image)) {
-            StorageUtil.saveSongImage(image, name);
-            songImageLocation = StorageUtil.getSongImagePath(name);
-        }
-
-        return new Song(
-                songLocation,
-                name,
-                artist,
-                albumName,
-                songImageLocation
-        );
+    private List<Uri> parseUriData(Uri data) {
+        return List.of(data);
     }
 }
