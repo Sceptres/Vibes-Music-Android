88098695728b0d79e41bfdece0aa42e880981139
Abdallah Alqashqish <alqashqa@mcmaster.ca>
2023-10-23 09:04:44 -0400

    Optimize imports

--- app/src/main/java/com/aaa/vibesmusic/ui/fragment/library/SongLibraryViewModel.java
+++ app/src/main/java/com/aaa/vibesmusic/ui/fragment/library/SongLibraryViewModel.java
@@ -5,7 +5,6 @@ import android.app.Application;
 import androidx.annotation.NonNull;
 import androidx.lifecycle.AndroidViewModel;
 import androidx.lifecycle.LiveData;
-import androidx.lifecycle.MutableLiveData;
 
 import com.aaa.vibesmusic.database.VibesMusicDatabase;
 import com.aaa.vibesmusic.database.data.music.Song;
