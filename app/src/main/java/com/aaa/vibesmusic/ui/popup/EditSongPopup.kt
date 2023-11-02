50e4a1d237fd17e340a1b3195c94719ddd2a1f6c
Abdallah Alqashqish <alqashqa@mcmaster.ca>
2023-11-01 22:22:00 -0400

    Inform user that the song was updated successfully

--- app/src/main/java/com/aaa/vibesmusic/ui/popup/EditSongPopup.kt
+++ app/src/main/java/com/aaa/vibesmusic/ui/popup/EditSongPopup.kt
@@ -4,28 +4,28 @@ import android.app.AlertDialog
 import android.app.Dialog
 import android.content.Context
 import android.os.Bundle
-import android.util.Log
 import android.view.LayoutInflater
 import android.view.View
 import android.view.inputmethod.InputMethodManager
 import android.widget.EditText
 import android.widget.TextView
-import androidx.appcompat.app.AppCompatDialog
 import androidx.appcompat.app.AppCompatDialogFragment
-import androidx.core.content.getSystemService
-import androidx.navigation.fragment.findNavController
 import com.aaa.vibesmusic.R
 import com.aaa.vibesmusic.database.VibesMusicDatabase
 import com.aaa.vibesmusic.database.data.music.Song
 import com.aaa.vibesmusic.ui.UIUtil
 import com.google.android.material.dialog.MaterialAlertDialogBuilder
 import io.reactivex.android.schedulers.AndroidSchedulers
+import io.reactivex.disposables.Disposable
 import io.reactivex.schedulers.Schedulers
+import java.util.Objects
 
 class EditSongPopup(
     private val c: Context,
     private val song: Song,
     private val db: VibesMusicDatabase) : AppCompatDialogFragment() {
+
+    private var updateDisposable: Disposable? = null
 
 
     override fun onCreateDialog(savedInstanceState: Bundle?): Dialog {
@@ -84,15 +84,27 @@ class EditSongPopup(
                 song.duration
             )
 
-            if(this.song != newSong)
-                this.db.songDao().updateSong(newSong)
+            if(this.song != newSong) {
+                updateDisposable = this.db.songDao().updateSong(newSong)
                     .subscribeOn(Schedulers.io())
                     .observeOn(AndroidSchedulers.mainThread())
-                    .subscribe()
+                    .subscribe {
+                        UIUtil.showLongSnackBar(
+                            "Song updated successfully.",
+                            resources.getColor(R.color.foreground_color, null)
+                        )
+                    }
+            }
 
             alertDialog.dismiss()
         }
 
         return alertDialog
+    }
+
+    override fun onDestroyView() {
+        super.onDestroyView()
+        if(Objects.nonNull(updateDisposable) && !updateDisposable!!.isDisposed)
+            updateDisposable!!.dispose()
     }
 }
