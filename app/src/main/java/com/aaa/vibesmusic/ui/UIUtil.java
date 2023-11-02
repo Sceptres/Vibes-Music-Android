05725ed7a48dfdc01574e39fbc01d2be0d533081
Abdallah Alqashqish <alqashqa@mcmaster.ca>
2023-11-01 21:46:21 -0400

    Use main activity fragment coordinatorview for snackbars

--- app/src/main/java/com/aaa/vibesmusic/ui/UIUtil.java
+++ app/src/main/java/com/aaa/vibesmusic/ui/UIUtil.java
@@ -6,7 +6,10 @@ import android.widget.Toast;
 
 import androidx.annotation.NonNull;
 
+import com.aaa.vibesmusic.ui.activity.MainActivity;
 import com.google.android.material.snackbar.Snackbar;
+
+import java.util.Objects;
 
 public class UIUtil {
     /**
@@ -20,12 +23,15 @@ public class UIUtil {
 
     /**
      *
-     * @param view The {@link View} to show the {@link Snackbar} on
      * @param msg The message the {@link Snackbar} should show
      * @param backgroundColor The background color of the {@link Snackbar}
      */
-    public static void showLongSnackBar(View view, String msg, int backgroundColor) {
-        Snackbar snackbar = Snackbar.make(view, msg, Snackbar.LENGTH_LONG);
+    public static void showLongSnackBar(String msg, int backgroundColor) {
+        Snackbar snackbar = Snackbar.make(
+                Objects.requireNonNull(MainActivity.Companion.getSNACK_BAR_VIEW()),
+                msg,
+                Snackbar.LENGTH_LONG
+        );
         snackbar.getView().setBackgroundColor(backgroundColor);
         snackbar.show();
     }
