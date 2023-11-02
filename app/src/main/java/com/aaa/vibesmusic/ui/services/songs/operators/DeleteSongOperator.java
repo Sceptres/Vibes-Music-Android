4dc8e2c757a3e87f9a0839f749cbb98efd82477e
Abdallah Alqashqish <alqashqa@mcmaster.ca>
2023-11-02 14:03:48 -0400

    Put delete song popup in class

--- app/src/main/java/com/aaa/vibesmusic/ui/services/songs/operators/DeleteSongOperator.java
+++ app/src/main/java/com/aaa/vibesmusic/ui/services/songs/operators/DeleteSongOperator.java
@@ -1,47 +1,22 @@
 package com.aaa.vibesmusic.ui.services.songs.operators;
 
-import android.content.Context;
-import android.content.DialogInterface;
+import androidx.fragment.app.FragmentManager;
 
-import androidx.appcompat.app.AlertDialog;
-
-import com.aaa.vibesmusic.R;
 import com.aaa.vibesmusic.database.VibesMusicDatabase;
 import com.aaa.vibesmusic.database.data.music.Song;
-import com.aaa.vibesmusic.storage.StorageUtil;
+import com.aaa.vibesmusic.ui.popup.DeleteSongPopup;
 import com.aaa.vibesmusic.ui.services.songs.SongMenuOperator;
-import com.google.android.material.dialog.MaterialAlertDialogBuilder;
-
-import java.util.Objects;
-
-import io.reactivex.android.schedulers.AndroidSchedulers;
-import io.reactivex.schedulers.Schedulers;
 
 public class DeleteSongOperator implements SongMenuOperator {
-    private final Context context;
+    private final FragmentManager manager;
 
-    public DeleteSongOperator(Context context) {
-        this.context = context;
+    public DeleteSongOperator(FragmentManager manager) {
+        this.manager = manager;
     }
 
     @Override
     public void operate(Song song, VibesMusicDatabase db) {
-        new MaterialAlertDialogBuilder(this.context, R.style.ConfirmDialogStyle)
-                .setTitle("Are you sure?")
-                .setMessage("Do you want to delete this song?")
-                .setPositiveButton("Delete", (dialog, which) -> {
-                    if (which == DialogInterface.BUTTON_POSITIVE) {
-                        StorageUtil.deleteFile(song.getLocation());
-                        if(Objects.nonNull(song.getImageLocation()))
-                            StorageUtil.deleteFile(song.getImageLocation());
-                        db.songDao()
-                                .deleteSong(song)
-                                .subscribeOn(Schedulers.io())
-                                .observeOn(AndroidSchedulers.mainThread())
-                                .subscribe();
-                    }
-                })
-                .setNegativeButton("Cancel", null)
-                .show();
+        DeleteSongPopup popup = new DeleteSongPopup(song, db);
+        popup.show(this.manager, "Delete Song");
     }
 }
