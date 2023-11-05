e0f03b532457acf4625d2403199ccca37c7b0da8
Abdallah Alqashqish <alqashqa@mcmaster.ca>
2023-11-04 22:43:50 -0400

    Impelement initial media player service

--- app/src/main/java/com/aaa/vibesmusic/ui/activity/MainActivity.kt
+++ app/src/main/java/com/aaa/vibesmusic/ui/activity/MainActivity.kt
@@ -1,6 +1,11 @@
 package com.aaa.vibesmusic.ui.activity
 
+import android.content.ComponentName
+import android.content.Intent
+import android.content.ServiceConnection
 import android.os.Bundle
+import android.os.IBinder
+import android.util.Log
 import androidx.appcompat.app.AppCompatActivity
 import androidx.coordinatorlayout.widget.CoordinatorLayout
 import androidx.navigation.fragment.NavHostFragment
@@ -8,10 +13,14 @@ import androidx.navigation.ui.setupWithNavController
 import com.aaa.vibesmusic.R
 import com.aaa.vibesmusic.database.VibesMusicDatabase
 import com.aaa.vibesmusic.databinding.ActivityMainBinding
+import com.aaa.vibesmusic.player.MediaPlayerService
 import com.aaa.vibesmusic.storage.StorageUtil
 import com.google.android.material.bottomnavigation.BottomNavigationView
 
-class MainActivity : AppCompatActivity() {
+class MainActivity : AppCompatActivity(), ServiceConnection {
+    var mediaPlayerService: MediaPlayerService? = null
+    var isServiceBound: Boolean = false
+
     private var _binding: ActivityMainBinding? = null
     private val binding
         get() = _binding!!
@@ -38,5 +47,30 @@ class MainActivity : AppCompatActivity() {
         bottomNav.setupWithNavController(navController)
 
         SNACK_BAR_VIEW = binding.fragmentContainer
+    }
+
+    override fun onStart() {
+        super.onStart()
+        val serviceIntent: Intent = Intent(this, MediaPlayerService::class.java)
+        this.application.bindService(serviceIntent, this, BIND_AUTO_CREATE)
+        this.application.startService(serviceIntent)
+    }
+
+    override fun onDestroy() {
+        super.onDestroy()
+        this.mediaPlayerService?.onDestroy()
+        this.application.unbindService(this)
+    }
+
+    override fun onServiceConnected(name: ComponentName?, service: IBinder?) {
+        val binder = service as MediaPlayerService.MediaPlayerServiceBinder
+        this.mediaPlayerService = binder.mediaPlayerService
+        this.isServiceBound = true
+        Log.d("SERVICE", "PLAYER SERVICE HAS BEEN CONNECTED")
+    }
+
+    override fun onServiceDisconnected(name: ComponentName?) {
+        this.mediaPlayerService = null
+        this.isServiceBound = false
     }
 }
--- app/src/main/java/com/aaa/vibesmusic/ui/fragment/library/SongLibraryFragment.kt
+++ app/src/main/java/com/aaa/vibesmusic/ui/fragment/library/SongLibraryFragment.kt
@@ -1,16 +1,25 @@
 package com.aaa.vibesmusic.ui.fragment.library
 
+import android.content.ComponentName
+import android.content.Intent
+import android.content.ServiceConnection
 import android.os.Bundle
+import android.os.IBinder
+import android.util.Log
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import androidx.appcompat.app.AppCompatActivity
 import androidx.fragment.app.Fragment
 import com.aaa.vibesmusic.database.VibesMusicDatabase
+import com.aaa.vibesmusic.database.data.music.Song
 import com.aaa.vibesmusic.databinding.FragmentSongLibraryBinding
+import com.aaa.vibesmusic.player.MediaPlayerService
 import com.aaa.vibesmusic.ui.adapters.SongsArrayAdapter
 
-class SongLibraryFragment : Fragment() {
+class SongLibraryFragment : Fragment(), ServiceConnection {
     private lateinit var viewModel: SongLibraryViewModel
+    private lateinit var mediaPlayerService: MediaPlayerService
 
     private var _binding: FragmentSongLibraryBinding? = null
     private val binding: FragmentSongLibraryBinding
@@ -36,6 +45,30 @@ class SongLibraryFragment : Fragment() {
             songsAdapter.notifyDataSetChanged()
         }
 
+        binding.songsListView.setOnItemClickListener { parent, view, position, id ->
+            this.mediaPlayerService.setSongs(songsAdapter.data, position)
+        }
+
         return binding.root
     }
+
+    override fun onStart() {
+        super.onStart()
+        val serviceIntent: Intent = Intent(this.requireContext(), MediaPlayerService::class.java)
+        this.requireActivity().application.bindService(serviceIntent, this, AppCompatActivity.BIND_AUTO_CREATE)
+        this.requireActivity().application.startService(serviceIntent)
+    }
+
+    override fun onDestroy() {
+        super.onDestroy()
+        this.mediaPlayerService.onDestroy()
+        this.requireActivity().application.unbindService(this)
+    }
+
+    override fun onServiceConnected(name: ComponentName?, service: IBinder?) {
+        val binder = service as MediaPlayerService.MediaPlayerServiceBinder
+        this.mediaPlayerService = binder.mediaPlayerService
+    }
+
+    override fun onServiceDisconnected(name: ComponentName?) {}
 }
