5c78fc6a437f40960cb4ad8dbd93f75ffa40f6b1
Abdallah Alqashqish <alqashqa@mcmaster.ca>
2023-11-16 22:19:06 -0500

    Document the SongPlayer class

--- app/src/main/java/com/aaa/vibesmusic/player/song/SongPlayer.java
+++ app/src/main/java/com/aaa/vibesmusic/player/song/SongPlayer.java
@@ -35,7 +35,12 @@ public class SongPlayer {
         this.applyShuffleMode();
     }
 
-    public synchronized void setSongs(List<Song> songs, int index) {
+    /**
+     *
+     * @param songs The {@link List} of {@link Song}s to add to this player
+     * @param index The index of the {@link Song} to start playing
+     */
+    public void setSongs(List<Song> songs, int index) {
         this.setOriginalSongs(songs);
         this.songs.clear();
         this.songs.addAll(songs);
@@ -44,15 +49,27 @@ public class SongPlayer {
         this.currentSongIndex = this.findSong(song);
     }
 
+    /**
+     *
+     * @param originalSongs Setter for {@link SongPlayer#originalSongs}
+     */
     private void setOriginalSongs(List<Song> originalSongs) {
         this.originalSongs.clear();
         this.originalSongs.addAll(originalSongs);
     }
 
+    /**
+     *
+     * @param playMode Set the {@link PlayMode} of this player
+     */
     public void setPlayMode(PlayMode playMode) {
         this.playMode = playMode;
     }
 
+    /**
+     *
+     * @param shuffleMode Set the {@link ShuffleMode} of this player
+     */
     public void setShuffleMode(ShuffleMode shuffleMode) {
         this.shuffleMode = shuffleMode;
         Song currentSong = null;
@@ -63,31 +80,58 @@ public class SongPlayer {
             this.currentSongIndex = this.songs.indexOf(currentSong);
     }
 
+    /**
+     *
+     * @param pauseTime Set the time at which the {@link Song} was paused
+     */
     public void setPauseTime(int pauseTime) {
         this.pauseTime = pauseTime;
     }
 
+    /**
+     *
+     * @return Get the time at which the {@link Song} was paused
+     */
     public int getPauseTime() {
         return this.pauseTime;
     }
 
+    /**
+     *
+     * @return True if this player has no songs. False otherwise.
+     */
     public boolean isEmpty() {
         return this.songs.isEmpty();
     }
 
+    /**
+     *
+     * @return The {@link List} of {@link Song}s in this player
+     */
     public List<Song> getSongs() {
-        return this.songs;
+        return new ArrayList<>(this.songs);
     }
 
+    /**
+     *
+     * @return The current {@link Song} being played
+     */
     public Song getCurrentSong() {
         return this.songs.get(this.currentSongIndex);
     }
 
+    /**
+     *
+     * @return Get the next {@link Song} to be played based on the {@link PlayMode} of this player
+     */
     public Song getNextSong() {
         this.currentSongIndex = this.playMode.getCalc().getNextSong(this.songs, this.currentSongIndex);
         return this.songs.get(this.currentSongIndex);
     }
 
+    /**
+     * Apply the shuffle mode of the player to the {@link List} of {@link Songs}
+     */
     private void applyShuffleMode() {
         switch (this.shuffleMode) {
             case SHUFFLED -> {
@@ -100,10 +144,18 @@ public class SongPlayer {
         }
     }
 
+    /**
+     *
+     * @param song The {@link Song} to look for
+     * @return The index of the {@link Song} in {@link SongPlayer#songs}
+     */
     private int findSong(Song song) {
         return this.songs.indexOf(song);
     }
 
+    /**
+     * Empty the player and reset
+     */
     public void reset() {
         this.currentSongIndex = 0;
         this.playMode = PlayMode.REPEAT;
