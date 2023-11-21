db19de70cc10dc8a2821127194c098809bff6c9e
Abdallah Alqashqish <alqashqa@mcmaster.ca>
2023-11-21 13:55:07 -0500

    Implement importing songs from thread

--- app/src/main/java/com/aaa/vibesmusic/ui/fragment/ImportSongsFragment.kt
+++ app/src/main/java/com/aaa/vibesmusic/ui/fragment/ImportSongsFragment.kt
@@ -7,30 +7,17 @@ import android.view.View
 import android.view.ViewGroup
 import androidx.activity.result.ActivityResultLauncher
 import androidx.fragment.app.Fragment
-import com.aaa.vibesmusic.R
 import com.aaa.vibesmusic.database.VibesMusicDatabase
 import com.aaa.vibesmusic.database.data.music.Song
-import com.aaa.vibesmusic.database.data.music.SongDao
 import com.aaa.vibesmusic.databinding.FragmentImportSongsBinding
-import com.aaa.vibesmusic.exceptions.SongAlreadyExistsException
-import com.aaa.vibesmusic.metadata.SongMetaData
-import com.aaa.vibesmusic.metadata.retriever.SongMetadataRetriever
 import com.aaa.vibesmusic.monetization.Ads
-import com.aaa.vibesmusic.storage.StorageUtil
-import com.aaa.vibesmusic.ui.UIUtil
 import com.aaa.vibesmusic.ui.fragment.result.ImportSongsActivityResultContract
-import com.google.android.gms.ads.AdError
 import com.google.android.gms.ads.FullScreenContentCallback
 import com.google.android.gms.ads.LoadAdError
 import com.google.android.gms.ads.interstitial.InterstitialAd
 import com.google.android.gms.ads.interstitial.InterstitialAdLoadCallback
-import io.reactivex.android.schedulers.AndroidSchedulers
 import io.reactivex.disposables.CompositeDisposable
-import io.reactivex.schedulers.Schedulers
-import java.io.FileDescriptor
-import java.io.IOException
 import java.util.Date
-import java.util.Objects
 
 class ImportSongsFragment : Fragment() {
     private var importSongsLauncher: ActivityResultLauncher<Void>? = null
@@ -52,24 +39,7 @@ class ImportSongsFragment : Fragment() {
                 val currentTime = Date().time
 
                 if(currentTime - lastShownAd >= Ads.IMPORT_AD_TIME_DIFF) {
-                    Ads.loadInterstitial(this.requireActivity(), Ads.IMPORT_MUSIC_AD_ID, object : InterstitialAdLoadCallback() {
-                        override fun onAdLoaded(p0: InterstitialAd) {
-                            super.onAdLoaded(p0)
-                            lastShownAd = currentTime
-                            p0.fullScreenContentCallback = object : FullScreenContentCallback() {
-                                override fun onAdDismissedFullScreenContent() {
-                                    super.onAdDismissedFullScreenContent()
-                                    importSongs(it)
-                                }
-                            }
-                            p0.show(requireActivity())
-                        }
-
-                        override fun onAdFailedToLoad(p0: LoadAdError) {
-                            super.onAdFailedToLoad(p0)
-                            importSongs(it)
-                        }
-                    })
+                    this.showImportSongAd(it, currentTime);
                 } else {
                     importSongs(it)
                 }
@@ -99,143 +69,34 @@ class ImportSongsFragment : Fragment() {
      * @param songs The [List] of [Song]s to import into the app
      */
     private fun importSongs(songs: List<Uri>) {
-        val importedSongs: List<Song> = importSongsFromUris(songs)
+        val importThread = ImportSongsThread(this.requireActivity().applicationContext, this.db, songs)
+        importThread.start()
+    }
 
-        // Were there any songs imported to be added to the database?
-        if (importedSongs.isNotEmpty()) {
-            mDisposable.add(
-                db!!.songDao().insertSongs(importedSongs)
-                    .subscribeOn(Schedulers.io())
-                    .observeOn(AndroidSchedulers.mainThread())
-                    .subscribe {
-                        UIUtil.showLongSnackBar(
-                            resources.getString(R.string.songs_imported_successfully),
-                            resources.getColor(R.color.foreground_color, null)
-                        )
+    /**
+     * @param it The [List] of [Uri]s selected by the user
+     * @param currentTime The time at which the request to show this ad was made.
+     */
+    private fun showImportSongAd(it: List<Uri>, currentTime: Long) {
+        Ads.loadInterstitial(this.requireActivity(), Ads.IMPORT_MUSIC_AD_ID, object : InterstitialAdLoadCallback() {
+            override fun onAdLoaded(p0: InterstitialAd) {
+                super.onAdLoaded(p0)
+                lastShownAd = currentTime
+                p0.fullScreenContentCallback = object : FullScreenContentCallback() {
+                    override fun onAdDismissedFullScreenContent() {
+                        super.onAdDismissedFullScreenContent()
+                        importSongs(it)
                     }
-            )
-        }
-    }
-
-    /**
-     *
-     * @param contentUri The [Uri] of the song to extract
-     * @return The [Song] instance that has been imported
-     * @throws IOException If there was an error copying the song
-     * @throws SongAlreadyExistsException If the song being imported already exists
-     */
-    @Throws(IOException::class, SongAlreadyExistsException::class)
-    private fun parseUri(contentUri: Uri, metaData: SongMetaData): Song {
-        val resolver = this.requireActivity().contentResolver
-        val fileName: String = StorageUtil.getFileName(resolver, contentUri)
-
-        val name = if(metaData.name.equals(SongMetadataRetriever.SONG_NAME_DEFAULT))
-            fileName
-        else
-            metaData.name
-        val artist = metaData.artist
-        val albumName = metaData.albumName
-        val image = metaData.image
-        val duration = metaData.duration
-
-        val songDao: SongDao = this.db!!.songDao()
-        if (songDao.doesSongExist(name, artist, albumName)) {
-            throw SongAlreadyExistsException()
-        }
-
-        var songLocation: String?
-        var songImageLocation: String? = null
-        var wasSongSaved: Boolean
-
-        resolver.openFileDescriptor(contentUri, "r").use { fd ->
-            val fileDescriptor: FileDescriptor = fd!!.fileDescriptor
-            wasSongSaved = StorageUtil.saveSong(fileDescriptor, fileName)
-            songLocation = StorageUtil.getSongPath(fileName)
-        }
-
-        if (!wasSongSaved)
-            throw IOException("Unable to save the song!")
-
-        if (Objects.nonNull(image)) {
-            StorageUtil.saveSongImage(image, name)
-            songImageLocation = StorageUtil.getSongImagePath(name)
-        }
-        return Song(
-            songLocation!!,
-            name,
-            artist,
-            albumName,
-            songImageLocation,
-            duration
-        )
-    }
-
-    /**
-     *
-     * @param uris The [List] of [Uri]s that have been chosen by the user
-     * @return The [List] of [Song]s that were imported to the application
-     */
-    private fun importSongsFromUris(uris: List<Uri>): List<Song> {
-        val songs: MutableList<Song> = ArrayList()
-        val context = this.requireActivity().applicationContext
-
-        var currentSong = ""
-        val failedSongs: MutableList<String> = ArrayList()
-        val existingSongs: MutableList<String> = ArrayList()
-
-        for (uri in uris) {
-            try {
-                SongMetadataRetriever(context, uri).use { retriever ->
-                    val metaData = retriever.allMetaData
-                    currentSong = metaData.name
-                    val song = this.parseUri(uri, metaData)
-                    songs.add(song)
                 }
-            } catch (e: IOException) {
-                failedSongs.add(currentSong)
-            } catch (e: SongAlreadyExistsException) {
-                existingSongs.add(currentSong)
+                p0.show(requireActivity())
             }
-        }
 
-        this.handleFailedSongs(failedSongs)
-        this.handleExistingSongs(existingSongs)
-
-        return songs
+            override fun onAdFailedToLoad(p0: LoadAdError) {
+                super.onAdFailedToLoad(p0)
+                importSongs(it)
+            }
+        })
     }
 
-    /**
-     * @param failedSongs The [List] of songs that were failed to be imported
-     */
-    private fun handleFailedSongs(failedSongs: List<String>) {
-        if (failedSongs.isNotEmpty()) {
-            val msg: String = if(failedSongs.size > 1)
-                resources.getString(R.string.failed_songs)
-            else
-                resources.getString(R.string.failed_song)
 
-            UIUtil.showLongSnackBar(
-                msg,
-                resources.getColor(R.color.foreground_color, null)
-            )
-        }
-    }
-
-    /**
-     * @param existingSongs The [List] of songs by their names that were deemed to have already been
-     * imported
-     */
-    private fun handleExistingSongs(existingSongs: List<String>) {
-        if (existingSongs.isNotEmpty()) {
-            val msg: String = if(existingSongs.size > 1) // Has more than one element?
-                resources.getString(R.string.songs_exist)
-            else
-                resources.getString(R.string.song_exists)
-
-            UIUtil.showLongSnackBar(
-                msg,
-                resources.getColor(R.color.foreground_color, null)
-            )
-        }
-    }
 }
