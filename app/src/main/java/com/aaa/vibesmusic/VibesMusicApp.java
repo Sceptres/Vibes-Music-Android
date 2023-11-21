e881e2b57cde77ea81bdc5e0692cf44f92e28cb6
Abdallah Alqashqish <alqashqa@mcmaster.ca>
2023-11-20 19:57:39 -0500

    Implement not showing ad on the first open of the app

--- app/src/main/java/com/aaa/vibesmusic/VibesMusicApp.java
+++ app/src/main/java/com/aaa/vibesmusic/VibesMusicApp.java
@@ -16,6 +16,7 @@ import androidx.appcompat.app.AppCompatActivity;
 
 import com.aaa.vibesmusic.monetization.Ads;
 import com.aaa.vibesmusic.player.MediaPlayerService;
+import com.aaa.vibesmusic.preferences.PreferencesManager;
 import com.aaa.vibesmusic.ui.activity.MainActivity;
 import com.google.android.gms.ads.AdError;
 import com.google.android.gms.ads.FullScreenContentCallback;
@@ -28,17 +29,20 @@ import java.util.Objects;
 
 public class VibesMusicApp extends Application implements Application.ActivityLifecycleCallbacks, ServiceConnection {
     private MediaPlayerService mediaPlayerService;
+    private PreferencesManager manager;
     private final ViewTreeObserver.OnPreDrawListener waitListener;
 
     public VibesMusicApp() {
         super();
         this.mediaPlayerService = null;
+        this.manager = null;
         this.waitListener = () -> false;
     }
 
     @Override
     public void onCreate() {
         super.onCreate();
+        this.manager = new PreferencesManager(this);
         this.registerActivityLifecycleCallbacks(this);
         MobileAds.initialize(this, initializationStatus -> {});
     }
@@ -46,38 +50,42 @@ public class VibesMusicApp extends Application implements Application.ActivityLi
     @Override
     public void onActivityCreated(@NonNull Activity activity, @Nullable Bundle savedInstanceState) {
         if(activity instanceof MainActivity) {
-            final View content = activity.findViewById(android.R.id.content);
-            content.getViewTreeObserver().addOnPreDrawListener(this.waitListener);
+            if(!this.manager.isFirstAppUse()) {
+                final View content = activity.findViewById(android.R.id.content);
+                content.getViewTreeObserver().addOnPreDrawListener(this.waitListener);
 
-            Ads.loadInterstitial(this.getApplicationContext(), Ads.OPEN_APP_AD_ID, new InterstitialAdLoadCallback() {
-                @Override
-                public void onAdFailedToLoad(@NonNull LoadAdError loadAdError) {
-                    super.onAdFailedToLoad(loadAdError);
-                    content.getViewTreeObserver().removeOnPreDrawListener(waitListener);
-                    content.getViewTreeObserver().addOnPreDrawListener(() -> true);
-                }
+                Ads.loadInterstitial(this.getApplicationContext(), Ads.OPEN_APP_AD_ID, new InterstitialAdLoadCallback() {
+                    @Override
+                    public void onAdFailedToLoad(@NonNull LoadAdError loadAdError) {
+                        super.onAdFailedToLoad(loadAdError);
+                        content.getViewTreeObserver().removeOnPreDrawListener(waitListener);
+                        content.getViewTreeObserver().addOnPreDrawListener(() -> true);
+                    }
 
-                @Override
-                public void onAdLoaded(@NonNull InterstitialAd interstitialAd) {
-                    super.onAdLoaded(interstitialAd);
-                    interstitialAd.setFullScreenContentCallback(new FullScreenContentCallback() {
-                        @Override
-                        public void onAdDismissedFullScreenContent() {
-                            super.onAdDismissedFullScreenContent();
-                            content.getViewTreeObserver().removeOnPreDrawListener(waitListener);
-                            content.getViewTreeObserver().addOnPreDrawListener(() -> true);
-                        }
+                    @Override
+                    public void onAdLoaded(@NonNull InterstitialAd interstitialAd) {
+                        super.onAdLoaded(interstitialAd);
+                        interstitialAd.setFullScreenContentCallback(new FullScreenContentCallback() {
+                            @Override
+                            public void onAdDismissedFullScreenContent() {
+                                super.onAdDismissedFullScreenContent();
+                                content.getViewTreeObserver().removeOnPreDrawListener(waitListener);
+                                content.getViewTreeObserver().addOnPreDrawListener(() -> true);
+                            }
 
-                        @Override
-                        public void onAdFailedToShowFullScreenContent(@NonNull AdError adError) {
-                            super.onAdFailedToShowFullScreenContent(adError);
-                            content.getViewTreeObserver().removeOnPreDrawListener(waitListener);
-                            content.getViewTreeObserver().addOnPreDrawListener(() -> true);
-                        }
-                    });
-                    interstitialAd.show(activity);
-                }
-            });
+                            @Override
+                            public void onAdFailedToShowFullScreenContent(@NonNull AdError adError) {
+                                super.onAdFailedToShowFullScreenContent(adError);
+                                content.getViewTreeObserver().removeOnPreDrawListener(waitListener);
+                                content.getViewTreeObserver().addOnPreDrawListener(() -> true);
+                            }
+                        });
+                        interstitialAd.show(activity);
+                    }
+                });
+            } else {
+                this.manager.setIsFirstAppUse(false);
+            }
 
             Intent serviceIntent = new Intent(this.getApplicationContext(), MediaPlayerService.class);
             this.bindService(serviceIntent, this, AppCompatActivity.BIND_AUTO_CREATE);
