87c36db0f9d32c5df0cf8fa4e4dcd418c76f0586
Abdallah Alqashqish <alqashqa@mcmaster.ca>
2024-08-27 18:28:27 -0400

    Implement song library fragment as a composable

---
+++ app/src/main/java/com/aaa/vibesmusic/ui/library/SongLibrary.kt
@@ -0,0 +1,225 @@
+package com.aaa.vibesmusic.ui.library
+
+import androidx.compose.foundation.Image
+import androidx.compose.foundation.background
+import androidx.compose.foundation.clickable
+import androidx.compose.foundation.layout.Arrangement
+import androidx.compose.foundation.layout.Box
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.Row
+import androidx.compose.foundation.layout.Spacer
+import androidx.compose.foundation.layout.fillMaxHeight
+import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.height
+import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.layout.size
+import androidx.compose.foundation.layout.width
+import androidx.compose.foundation.layout.wrapContentHeight
+import androidx.compose.foundation.layout.wrapContentSize
+import androidx.compose.foundation.lazy.LazyColumn
+import androidx.compose.foundation.lazy.items
+import androidx.compose.foundation.shape.CircleShape
+import androidx.compose.foundation.shape.RoundedCornerShape
+import androidx.compose.material3.FloatingActionButton
+import androidx.compose.material3.Icon
+import androidx.compose.material3.IconButton
+import androidx.compose.material3.Text
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Alignment
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.draw.clip
+import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.res.colorResource
+import androidx.compose.ui.res.painterResource
+import androidx.compose.ui.text.font.FontFamily
+import androidx.compose.ui.text.font.FontWeight
+import androidx.compose.ui.text.style.TextOverflow
+import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.dp
+import androidx.compose.ui.unit.sp
+import androidx.constraintlayout.compose.ConstraintLayout
+import androidx.constraintlayout.compose.Dimension
+import com.aaa.vibesmusic.R
+import com.aaa.vibesmusic.database.data.music.Song
+import com.aaa.vibesmusic.ui.monetization.AdmobBanner;
+import java.util.Objects
+
+@Composable
+@Preview(showBackground = true)
+fun SongLibrary() {
+    ConstraintLayout {
+        val (mainBody, adview) = createRefs()
+
+        Box(
+            modifier = Modifier
+                .background(color = colorResource(R.color.background_color))
+                .constrainAs(mainBody) {
+                    top.linkTo(parent.top)
+                    start.linkTo(parent.start)
+                    end.linkTo(parent.end)
+                    bottom.linkTo(adview.top)
+                    width = Dimension.preferredWrapContent
+                    height = Dimension.preferredWrapContent
+                }
+        ) {
+            Column(
+                modifier = Modifier.align(Alignment.TopStart)
+            ) {
+                Text(
+                    text = "Music Library",
+                    fontFamily = FontFamily.SansSerif,
+                    fontWeight = FontWeight.Light,
+                    fontSize = 50.sp,
+                    color = Color.White,
+                    modifier = Modifier
+                        .padding(start = 10.dp, top = 5.dp)
+                )
+
+                SongsList(
+                    songs = arrayListOf(),
+                    modifier = Modifier.padding(top = 20.dp, start = 10.dp, end = 10.dp)
+                ) {}
+            }
+            SongPlayerFloatingButton(
+                modifier = Modifier
+                    .align(Alignment.BottomEnd)
+                    .padding(end = 10.dp, bottom = 10.dp)
+                    .wrapContentSize()
+            ) {
+
+            }
+        }
+        AdmobBanner(
+            adId = "***REMOVED***",
+            modifier = Modifier
+                .wrapContentSize()
+                .constrainAs(adview) {
+                    start.linkTo(parent.start)
+                    end.linkTo(parent.end)
+                    bottom.linkTo(parent.bottom)
+                }
+        )
+    }
+}
+
+@Composable
+fun SongsList(songs: List<Song>, modifier: Modifier = Modifier, onClick: () -> Unit) {
+    LazyColumn(
+        verticalArrangement = Arrangement.spacedBy(5.dp),
+        modifier = modifier
+    ) {
+        items(songs) { song ->
+            SongListItem(song, onClick, {})
+        }
+    }
+}
+
+@Composable
+fun SongPlayerFloatingButton(modifier: Modifier = Modifier, onClick: () -> Unit) {
+    FloatingActionButton(
+        onClick = { onClick.invoke() },
+        containerColor = colorResource(id = R.color.blue_selected),
+        content = {
+            Icon(
+                painter = painterResource(id = R.drawable.play_arrow),
+                contentDescription = "Playing songs page",
+                tint = Color.White,
+                modifier = Modifier.size(50.dp)
+            )
+        },
+        shape = CircleShape,
+        modifier = modifier
+    )
+}
+
+@Composable
+fun SongListItem(song: Song, onItemClick: () -> Unit, onOptionsClick: () -> Unit) {
+    Box(
+        modifier = Modifier
+            .fillMaxWidth()
+            .height(60.dp)
+            .clip(RoundedCornerShape(10.dp))
+            .background(colorResource(id = R.color.foreground_color))
+            .clickable { onItemClick.invoke() }
+    ) {
+        Row(
+            modifier = Modifier
+                .background(colorResource(id = R.color.foreground_color))
+        ) {
+            Image(
+                painter = if(Objects.isNull(song.imageLocation))
+                    painterResource(id = R.drawable.music_cover_image)
+                else
+                    painterResource(id = R.drawable.music_cover_image),
+                contentDescription = null,
+                modifier = Modifier
+                    .fillMaxHeight()
+                    .width(60.dp)
+                    .padding(all = 5.dp)
+                    .clip(RoundedCornerShape(10.dp))
+            )
+
+            Spacer(modifier = Modifier.width(20.dp))
+
+            Column(
+                modifier = Modifier
+                    .weight(1f)
+                    .fillMaxHeight()
+            ) {
+                Text(
+                    text = song.name,
+                    color = Color.White,
+                    fontSize = 18.sp,
+                    maxLines = 1,
+                    overflow = TextOverflow.Ellipsis,
+                    modifier = Modifier
+                        .fillMaxWidth()
+                        .height(34.dp)
+                        .wrapContentHeight()
+                )
+
+                Text(
+                    text = song.artist,
+                    color = Color.White,
+                    fontSize = 11.sp,
+                    maxLines = 1,
+                    overflow = TextOverflow.Ellipsis,
+                    modifier = Modifier
+                        .fillMaxWidth()
+                        .height(26.dp)
+                        .wrapContentHeight()
+                )
+            }
+
+            Column(
+                horizontalAlignment = Alignment.CenterHorizontally,
+                modifier = Modifier
+                    .padding(vertical = 5.dp)
+            ) {
+                Text(
+                    text = "03:40:23",
+                    color = Color.White,
+                    fontSize = 11.sp,
+                    maxLines = 1,
+                    overflow = TextOverflow.Ellipsis,
+                    modifier = Modifier
+                        .fillMaxHeight()
+                        .wrapContentHeight()
+                )
+            }
+
+            IconButton(
+                onClick = { onOptionsClick.invoke() },
+                modifier = Modifier
+                    .fillMaxHeight()
+            ) {
+                Icon(
+                    painter = painterResource(id = R.drawable.options_btn),
+                    contentDescription = "Options",
+                    tint = Color.White,
+                    modifier = Modifier.fillMaxHeight()
+                )
+            }
+        }
+    }
+}
