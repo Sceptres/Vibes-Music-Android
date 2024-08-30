636305140f72ef31ae7aa0ef41e563a5b3c89304
Abdallah Alqashqish <alqashqa@mcmaster.ca>
2024-08-29 20:16:47 -0400

    Implement delete song dropdown item Implement deleting a song from the song list dropdown item using the DeleteSongDialog

---
+++ app/src/main/java/com/aaa/vibesmusic/ui/dialogs/delete/song/DeleteSongDialog.kt
@@ -0,0 +1,54 @@
+package com.aaa.vibesmusic.ui.dialogs.delete.song
+
+import androidx.compose.material3.SnackbarHostState
+import androidx.compose.runtime.Composable
+import androidx.compose.runtime.MutableState
+import androidx.lifecycle.viewmodel.compose.viewModel
+import com.aaa.vibesmusic.database.data.music.Song
+import com.aaa.vibesmusic.ui.UIUtil
+import com.aaa.vibesmusic.ui.dialogs.confirm.ConfirmAlertDialog
+import kotlinx.coroutines.CoroutineScope
+
+@Composable
+fun DeleteSongDialog(
+    song: Song,
+    dialogState: MutableState<Boolean>,
+    snackBarState: SnackbarHostState,
+    snackBarScope: CoroutineScope
+) {
+    val deleteSongDialogViewModel: DeleteSongDialogViewModel = viewModel(factory = DeleteSongDialogViewModel.FACTORY)
+
+    val onDismiss = {
+        dialogState.value = false
+    }
+
+    ConfirmAlertDialog(
+        title = "Are you sure?",
+        text = "Are you sure you want to delete ${song.name}?",
+        dismissButtonText = "Cancel",
+        onDismiss = {
+            onDismiss()
+        },
+        confirmButtonText = "Delete",
+        onConfirm = {
+            onDismiss()
+            deleteSongDialogViewModel.deleteSong(
+                song = song,
+                onSuccess = {
+                    UIUtil.showSnackBar(
+                        snackBarScope = snackBarScope,
+                        snackBarState = snackBarState,
+                        message = "The song ${song.name} has been successfully deleted!"
+                    )
+                },
+                onFail = {
+                    UIUtil.showSnackBar(
+                        snackBarScope = snackBarScope,
+                        snackBarState = snackBarState,
+                        message = "Failed to delete the song ${song.name}. Please try again!"
+                    )
+                }
+            )
+        }
+    )
+}
