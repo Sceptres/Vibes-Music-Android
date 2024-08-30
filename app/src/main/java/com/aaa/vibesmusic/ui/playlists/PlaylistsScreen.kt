9deef7ebc5879437eb353c570a61dc170d36514d
Abdallah Alqashqish <alqashqa@mcmaster.ca>
2024-08-30 17:29:28 -0400

    Implement playlist screen Implement displaying playlists from the database on the screen

--- app/src/main/java/com/aaa/vibesmusic/ui/playlists/PlaylistsScreen.kt
+++ app/src/main/java/com/aaa/vibesmusic/ui/playlists/PlaylistsScreen.kt
@@ -1,50 +1,43 @@
 package com.aaa.vibesmusic.ui.playlists
 
-import androidx.compose.foundation.Image
 import androidx.compose.foundation.background
 import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.PaddingValues
-import androidx.compose.foundation.layout.fillMaxHeight
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.fillMaxWidth
-import androidx.compose.foundation.layout.height
 import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.layout.size
-import androidx.compose.foundation.layout.wrapContentSize
 import androidx.compose.foundation.lazy.grid.GridCells
 import androidx.compose.foundation.lazy.grid.LazyVerticalGrid
+import androidx.compose.foundation.lazy.grid.items
 import androidx.compose.foundation.shape.CircleShape
-import androidx.compose.foundation.shape.RoundedCornerShape
 import androidx.compose.material3.FloatingActionButton
 import androidx.compose.material3.Icon
-import androidx.compose.material3.IconButton
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
-import androidx.compose.ui.draw.clip
 import androidx.compose.ui.graphics.Color
-import androidx.compose.ui.layout.ContentScale
 import androidx.compose.ui.res.colorResource
 import androidx.compose.ui.res.painterResource
 import androidx.compose.ui.res.stringResource
 import androidx.compose.ui.text.font.FontFamily
 import androidx.compose.ui.text.font.FontWeight
-import androidx.compose.ui.text.style.TextAlign
-import androidx.compose.ui.text.style.TextOverflow
-import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
 import androidx.compose.ui.unit.sp
 import androidx.constraintlayout.compose.ConstraintLayout
 import androidx.constraintlayout.compose.Dimension
+import androidx.lifecycle.viewmodel.compose.viewModel
 import com.aaa.vibesmusic.R
 import com.aaa.vibesmusic.ui.monetization.AdmobBanner
+import com.aaa.vibesmusic.ui.playlists.composables.PlaylistCard
 
 @Composable
-@Preview(showBackground = true)
 fun PlaylistsScreen() {
+    val playlistsScreenViewModel: PlaylistsScreenViewModel = viewModel(factory = PlaylistsScreenViewModel.FACTORY)
+
     val blueSelected = colorResource(id = R.color.blue_selected)
 
     ConstraintLayout(
@@ -89,8 +82,8 @@ fun PlaylistsScreen() {
                         .fillMaxWidth()
                         .weight(1f)
                 ) {
-                    items(10) { index ->
-                        PlaylistCard()
+                    items(playlistsScreenViewModel.playlistSongs) { playlistSong ->
+                        PlaylistCard(playlistSong)
                     }
                 }
             }
@@ -122,81 +115,5 @@ fun PlaylistsScreen() {
                 }
                 .fillMaxWidth()
         )
-    }
-}
-
-
-@Composable
-fun PlaylistCard() {
-    val white = colorResource(id = R.color.white)
-    val transparent = colorResource(id = R.color.transparent)
-
-    Box(
-        modifier = Modifier
-            .wrapContentSize()
-            .height(300.dp)
-            .clip(RoundedCornerShape(30.dp))
-            .background(colorResource(id = R.color.foreground_color))
-            .padding(10.dp)
-    ) {
-        Column(
-            modifier = Modifier
-                .fillMaxHeight()
-                .align(Alignment.Center)
-        ) {
-            Box(
-                modifier = Modifier
-                    .size(200.dp)
-                    .clip(RoundedCornerShape(10.dp))
-                    .background(colorResource(id = R.color.foreground_color))
-                    .align(Alignment.CenterHorizontally)
-            ) {
-                Image(
-                    painter = painterResource(id = R.drawable.music_cover_image),
-                    contentDescription = "Playlist Cover",
-                    contentScale = ContentScale.Crop,
-                    modifier = Modifier
-                        .fillMaxSize()
-                        .clip(RoundedCornerShape(30.dp))
-                )
-
-                IconButton(
-                    onClick = { /* handle options click */ },
-                    modifier = Modifier
-                        .size(45.dp)
-                        .align(Alignment.BottomEnd)
-                        .padding(end = 10.dp)
-                        .background(transparent)
-                ) {
-                    Icon(
-                        painter = painterResource(id = R.drawable.options_btn),
-                        contentDescription = "Options",
-                        tint = white
-                    )
-                }
-            }
-
-            Text(
-                text = "Playlist Name",
-                color = white,
-                fontSize = 20.sp,
-                maxLines = 1,
-                overflow = TextOverflow.Ellipsis,
-                textAlign = TextAlign.Center,
-                modifier = Modifier
-                    .padding(top = 10.dp)
-                    .align(Alignment.CenterHorizontally)
-            )
-
-            Text(
-                text = "6 hours, 40 minutes",
-                color = white,
-                fontSize = 15.sp,
-                textAlign = TextAlign.Center,
-                modifier = Modifier
-                    .padding(top = 5.dp, bottom = 10.dp)
-                    .align(Alignment.CenterHorizontally)
-            )
-        }
     }
 }
