6a1b2cbe5cbe4ed60699b5dfd8f7c8b9bdc124c3
Abdallah Alqashqish <alqashqa@mcmaster.ca>
2024-08-31 11:02:03 -0400

    Implement closing playing song screen on back press

--- app/src/main/java/com/aaa/vibesmusic/ui/activity/MainActivity.kt
+++ app/src/main/java/com/aaa/vibesmusic/ui/activity/MainActivity.kt
@@ -7,6 +7,7 @@ import android.content.pm.PackageManager
 import android.os.Bundle
 import android.os.IBinder
 import android.util.Log
+import androidx.activity.OnBackPressedCallback
 import androidx.activity.compose.setContent
 import androidx.appcompat.app.AppCompatActivity
 import androidx.compose.animation.AnimatedVisibility
@@ -41,6 +42,7 @@ import androidx.compose.runtime.setValue
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.graphics.toArgb
+import androidx.compose.ui.platform.LocalLifecycleOwner
 import androidx.compose.ui.res.colorResource
 import androidx.compose.ui.res.painterResource
 import androidx.compose.ui.res.stringResource
@@ -123,6 +125,18 @@ class MainActivity : AppCompatActivity(), ServiceConnection {
                 closeScreen = { playingSongScreenState.targetState = false }
             )
             navBarColorState = backgroundColor
+
+            // Ensure swiping back closes the playing song screen
+            onBackPressedDispatcher.addCallback(LocalLifecycleOwner.current, object: OnBackPressedCallback(true) {
+                override fun handleOnBackPressed() {
+                    if(playingSongScreenState.currentState) {
+                        playingSongScreenState.targetState = false
+                    } else {
+                        isEnabled = false
+                        moveTaskToBack(true)
+                    }
+                }
+            })
         }
     }
 
