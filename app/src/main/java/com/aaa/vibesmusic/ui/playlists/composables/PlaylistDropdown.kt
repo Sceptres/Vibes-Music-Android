723be3052ab26b307a99979d974080da8401f5c5
Abdallah Alqashqish <alqashqa@mcmaster.ca>
2024-08-31 00:56:39 -0400

    Implemented playlists dropdown menu UI Just implemented the UI of the dropdown menu. All that is left is implementing the dialogs that are shown when they are clicked

---
+++ app/src/main/java/com/aaa/vibesmusic/ui/playlists/composables/PlaylistDropdown.kt
@@ -0,0 +1,53 @@
+package com.aaa.vibesmusic.ui.playlists.composables
+
+import androidx.compose.material3.SnackbarHostState
+import androidx.compose.runtime.Composable
+import androidx.compose.runtime.MutableState
+import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.remember
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.res.stringResource
+import com.aaa.vibesmusic.R
+import com.aaa.vibesmusic.database.data.playlist.PlaylistSongs
+import com.aaa.vibesmusic.ui.common.CustomDropdown
+import com.aaa.vibesmusic.ui.common.CustomDropdownMenuItem
+import kotlinx.coroutines.CoroutineScope
+
+@Composable
+fun PlaylistDropdown(
+    expandedState: MutableState<Boolean>,
+    playlistSongs: PlaylistSongs,
+    snackBarState: SnackbarHostState,
+    snackBarScope: CoroutineScope,
+    modifier: Modifier = Modifier
+) {
+    val editDialogState: MutableState<Boolean> = remember { mutableStateOf(false) }
+    val deleteDialogState: MutableState<Boolean> = remember { mutableStateOf(false) }
+
+    when {
+        editDialogState.value -> {}
+        deleteDialogState.value -> {}
+    }
+
+    CustomDropdown(
+        expanded = expandedState.value,
+        onDismiss = { expandedState.value = false },
+        modifier = modifier
+    ) {
+        CustomDropdownMenuItem(
+            text = stringResource(id = R.string.delete),
+            onClick = {
+                expandedState.value = false
+                deleteDialogState.value = true
+            }
+        )
+
+        CustomDropdownMenuItem(
+            text = stringResource(id = R.string.edit_playlist),
+            onClick = {
+                expandedState.value = false
+                editDialogState.value = true
+            }
+        )
+    }
+}
