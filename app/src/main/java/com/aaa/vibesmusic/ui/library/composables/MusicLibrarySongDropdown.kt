d418232a1af04ab060b12c02f4cfeeb63c22bcf2
Abdallah Alqashqish <alqashqa@mcmaster.ca>
2024-08-31 00:55:46 -0400

    Remove unused imports

--- app/src/main/java/com/aaa/vibesmusic/ui/library/composables/MusicLibrarySongDropdown.kt
+++ app/src/main/java/com/aaa/vibesmusic/ui/library/composables/MusicLibrarySongDropdown.kt
@@ -1,15 +1,11 @@
 package com.aaa.vibesmusic.ui.library.composables
 
-import androidx.compose.material3.DropdownMenu
-import androidx.compose.material3.DropdownMenuItem
 import androidx.compose.material3.SnackbarHostState
-import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.MutableState
 import androidx.compose.runtime.mutableStateOf
 import androidx.compose.runtime.remember
 import androidx.compose.ui.Modifier
-import androidx.compose.ui.graphics.Color
 import com.aaa.vibesmusic.database.data.music.Song
 import com.aaa.vibesmusic.ui.common.CustomDropdown
 import com.aaa.vibesmusic.ui.common.CustomDropdownMenuItem
