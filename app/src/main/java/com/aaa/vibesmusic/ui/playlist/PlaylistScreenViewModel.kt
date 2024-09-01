556d823fb5de99f2d46d0f3b8edc47d40c884bc8
Abdallah Alqashqish <alqashqa@mcmaster.ca>
2024-09-01 19:46:47 -0400

    Implement clicking song in songs list

--- app/src/main/java/com/aaa/vibesmusic/ui/playlist/PlaylistScreenViewModel.kt
+++ app/src/main/java/com/aaa/vibesmusic/ui/playlist/PlaylistScreenViewModel.kt
@@ -1,6 +1,15 @@
 package com.aaa.vibesmusic.ui.playlist
 
+import android.Manifest
 import android.app.Application
+import android.content.ComponentName
+import android.content.Context
+import android.content.ServiceConnection
+import android.os.IBinder
+import androidx.activity.compose.ManagedActivityResultLauncher
+import androidx.activity.compose.rememberLauncherForActivityResult
+import androidx.activity.result.contract.ActivityResultContracts
+import androidx.compose.runtime.Composable
 import androidx.compose.runtime.getValue
 import androidx.compose.runtime.mutableStateOf
 import androidx.compose.runtime.setValue
@@ -12,7 +21,11 @@ import androidx.lifecycle.ViewModelProvider.AndroidViewModelFactory.Companion.AP
 import androidx.lifecycle.viewmodel.initializer
 import androidx.lifecycle.viewmodel.viewModelFactory
 import com.aaa.vibesmusic.database.VibesMusicDatabase
+import com.aaa.vibesmusic.database.data.music.Song
 import com.aaa.vibesmusic.database.data.playlist.PlaylistSongs
+import com.aaa.vibesmusic.perms.PermissionsUtil
+import com.aaa.vibesmusic.player.MediaPlayerService
+import java.util.Objects
 
 class PlaylistScreenViewModel(application: Application, playlistId: Int) : AndroidViewModel(application) {
     companion object {
@@ -27,6 +40,20 @@ class PlaylistScreenViewModel(application: Application, playlistId: Int) : Andro
 
     private val db: VibesMusicDatabase = VibesMusicDatabase.getInstance(this.getApplication())
 
+    // Player service
+    private var playerService: MediaPlayerService? by mutableStateOf(null)
+    private val serviceConnection: ServiceConnection = object : ServiceConnection {
+        override fun onServiceConnected(name: ComponentName?, service: IBinder?) {
+            val binder: MediaPlayerService.MediaPlayerServiceBinder = service as MediaPlayerService.MediaPlayerServiceBinder
+            playerService = binder.mediaPlayerService
+        }
+
+        override fun onServiceDisconnected(name: ComponentName?) {
+            playerService = null
+        }
+
+    }
+
     // Playlist songs state
     var playlistSongs: PlaylistSongs? by mutableStateOf(null)
     private var playlistSongsLiveData: LiveData<PlaylistSongs> = this.getPlaylistSongsLiveData(playlistId)
@@ -36,6 +63,31 @@ class PlaylistScreenViewModel(application: Application, playlistId: Int) : Andro
 
     init {
         this.playlistSongsLiveData.observeForever(this.playlistSongObserver)
+        this.initPlayerService()
+    }
+
+    fun onSongClicked(launcher: ManagedActivityResultLauncher<String, Boolean>, context: Context, index: Int) {
+        if(!PermissionsUtil.hasPermission(context, Manifest.permission.POST_NOTIFICATIONS))
+            launcher.launch(Manifest.permission.POST_NOTIFICATIONS)
+        this.playerService?.setSongs(this.getPlaylistSongs(), index)
+    }
+
+    fun getPlaylistSongs(): List<Song> {
+        return this.playlistSongs?.songs ?: listOf()
+    }
+
+    @Composable
+    fun getNotificationsPermissionLauncher(): ManagedActivityResultLauncher<String, Boolean> {
+        return rememberLauncherForActivityResult(contract = ActivityResultContracts.RequestPermission()) { isGranted ->
+            if(playerService?.isPlaying == true && isGranted) {
+                playerService?.showNotification()
+            }
+        }
+    }
+
+    private fun initPlayerService() {
+        if(Objects.isNull(this.playerService))
+            MediaPlayerService.bindTo(this.getApplication(), this.serviceConnection)
     }
 
     private fun getPlaylistSongsLiveData(playlistId: Int): LiveData<PlaylistSongs> {
@@ -45,5 +97,6 @@ class PlaylistScreenViewModel(application: Application, playlistId: Int) : Andro
     override fun onCleared() {
         super.onCleared()
         this.playlistSongsLiveData.removeObserver(this.playlistSongObserver)
+        getApplication<Application>().unbindService(this.serviceConnection)
     }
 }
