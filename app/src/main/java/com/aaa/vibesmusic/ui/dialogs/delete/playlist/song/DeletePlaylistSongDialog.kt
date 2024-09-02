7bffec6eaf1d1f7224535b736bf1242d6dac3b25
Abdallah Alqashqish <alqashqa@mcmaster.ca>
2024-09-02 01:58:56 -0400

    Implement removing song from playlist

---
+++ app/src/main/java/com/aaa/vibesmusic/ui/dialogs/delete/playlist/song/DeletePlaylistSongDialog.kt
@@ -0,0 +1,55 @@
+package com.aaa.vibesmusic.ui.dialogs.delete.playlist.song
+
+import androidx.compose.material3.SnackbarHostState
+import androidx.compose.runtime.Composable
+import androidx.compose.runtime.MutableState
+import androidx.lifecycle.viewmodel.compose.viewModel
+import com.aaa.vibesmusic.database.data.music.Song
+import com.aaa.vibesmusic.database.data.playlist.Playlist
+import com.aaa.vibesmusic.database.data.playlist.PlaylistSongs
+import com.aaa.vibesmusic.ui.UIUtil
+import com.aaa.vibesmusic.ui.dialogs.confirm.ConfirmAlertDialog
+import kotlinx.coroutines.CoroutineScope
+
+@Composable
+fun DeletePlaylistSongDialog(
+    dialogState: MutableState<Boolean>,
+    playlistSongs: PlaylistSongs,
+    song: Song,
+    snackBarState: SnackbarHostState,
+    snackBarScope: CoroutineScope
+) {
+    val deletePlaylistSongViewModel: DeletePlaylistSongDialogViewModel = viewModel(factory = DeletePlaylistSongDialogViewModel.FACTORY)
+    val playlist: Playlist = playlistSongs.playlist
+
+    val onDismiss: () -> Unit = { dialogState.value = false }
+
+    ConfirmAlertDialog(
+        title = "Are you sure?",
+        text = "Are you sure you want to remove ${song.name} from the ${playlist.name} playlist?",
+        onDismiss = onDismiss,
+        dismissButtonText = "Cancel",
+        onConfirm = {
+            onDismiss()
+            deletePlaylistSongViewModel.deletePlaylistSong(
+                playlist = playlist,
+                song = song,
+                onSuccess = {
+                    UIUtil.showSnackBar(
+                        snackBarScope = snackBarScope,
+                        snackBarState = snackBarState,
+                        message = "The song ${song.name} has been successfully removed from this playlist!"
+                    )
+                },
+                onFail = {
+                    UIUtil.showSnackBar(
+                        snackBarScope = snackBarScope,
+                        snackBarState = snackBarState,
+                        message = "Failed to remove ${song.name} from this playlist. Please try again!"
+                    )
+                }
+            )
+        },
+        confirmButtonText = "Remove"
+    )
+}
---
+++ app/src/main/java/com/aaa/vibesmusic/ui/dialogs/delete/playlist/song/DeletePlaylistSongDialogViewModel.kt
@@ -0,0 +1,48 @@
+package com.aaa.vibesmusic.ui.dialogs.delete.playlist.song
+
+import android.app.Application
+import androidx.compose.runtime.getValue
+import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.setValue
+import androidx.lifecycle.AndroidViewModel
+import androidx.lifecycle.ViewModelProvider
+import androidx.lifecycle.ViewModelProvider.AndroidViewModelFactory.Companion.APPLICATION_KEY
+import androidx.lifecycle.viewmodel.initializer
+import androidx.lifecycle.viewmodel.viewModelFactory
+import com.aaa.vibesmusic.database.VibesMusicDatabase
+import com.aaa.vibesmusic.database.data.music.Song
+import com.aaa.vibesmusic.database.data.playlist.Playlist
+import com.aaa.vibesmusic.database.data.playlist.PlaylistSongs
+import com.aaa.vibesmusic.database.data.relationships.playlist.PlaylistSongRelationship
+import io.reactivex.android.schedulers.AndroidSchedulers
+import io.reactivex.disposables.CompositeDisposable
+import io.reactivex.schedulers.Schedulers
+
+class DeletePlaylistSongDialogViewModel(application: Application) : AndroidViewModel(application) {
+    companion object {
+        val FACTORY: ViewModelProvider.Factory = viewModelFactory {
+            initializer {
+                DeletePlaylistSongDialogViewModel(this[APPLICATION_KEY] as Application)
+            }
+        }
+    }
+
+    private val db: VibesMusicDatabase = VibesMusicDatabase.getInstance(this.getApplication())
+    private val disposables: CompositeDisposable = CompositeDisposable()
+
+    fun deletePlaylistSong(playlist: Playlist, song: Song, onSuccess: () -> Unit, onFail: (Throwable) -> Unit) {
+        val playlistSongsRelationship: PlaylistSongRelationship = PlaylistSongRelationship(playlist.playlistId, song.songId)
+        this.disposables.add(
+            this.db.playlistSongRelationshipDao()
+                .deletePlaylistSongRelationship(playlistSongsRelationship)
+                .subscribeOn(Schedulers.io())
+                .observeOn(AndroidSchedulers.mainThread())
+                .subscribe(onSuccess, onFail)
+        )
+    }
+
+    override fun onCleared() {
+        super.onCleared()
+        this.disposables.clear()
+    }
+}
