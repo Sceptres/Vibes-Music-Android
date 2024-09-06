00941ed86e378112f20e1307f1127b09ec715aaa
Abdallah Alqashqish <alqashqa@mcmaster.ca>
2024-09-06 18:22:11 -0400

    Change delete playlist dialog view model to state holder object

--- app/src/main/java/com/aaa/vibesmusic/ui/dialogs/playlist/delete/DeletePlaylistDialogState.kt
+++ app/src/main/java/com/aaa/vibesmusic/ui/dialogs/playlist/delete/DeletePlaylistDialogState.kt
@@ -1,11 +1,6 @@
 package com.aaa.vibesmusic.ui.dialogs.playlist.delete
 
-import android.app.Application
-import androidx.lifecycle.AndroidViewModel
-import androidx.lifecycle.ViewModelProvider
-import androidx.lifecycle.ViewModelProvider.AndroidViewModelFactory.Companion.APPLICATION_KEY
-import androidx.lifecycle.viewmodel.initializer
-import androidx.lifecycle.viewmodel.viewModelFactory
+import android.content.Context
 import com.aaa.vibesmusic.database.VibesMusicDatabase
 import com.aaa.vibesmusic.database.data.playlist.PlaylistSongs
 import com.aaa.vibesmusic.database.util.DatabaseUtil
@@ -13,16 +8,8 @@ import io.reactivex.android.schedulers.AndroidSchedulers
 import io.reactivex.disposables.CompositeDisposable
 import io.reactivex.schedulers.Schedulers
 
-class DeletePlaylistDialogViewModel(application: Application) : AndroidViewModel(application) {
-    companion object {
-        val FACTORY: ViewModelProvider.Factory = viewModelFactory {
-            initializer {
-                DeletePlaylistDialogViewModel(this[APPLICATION_KEY] as Application)
-            }
-        }
-    }
-
-    private val db: VibesMusicDatabase = VibesMusicDatabase.getInstance(application)
+class DeletePlaylistDialogState(val context: Context) {
+    private val db: VibesMusicDatabase = VibesMusicDatabase.getInstance(this.context)
     private val disposables: CompositeDisposable = CompositeDisposable()
 
     fun deletePlaylistSongs(
@@ -37,10 +24,4 @@ class DeletePlaylistDialogViewModel(application: Application) : AndroidViewModel
                 .subscribe(onSuccess, onFail)
         )
     }
-
-    override fun onCleared() {
-        super.onCleared()
-        this.disposables.clear()
-    }
-
 }
