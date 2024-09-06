72e7574f61a799aa6d4fa8987d05d714b5998ff5
Abdallah Alqashqish <alqashqa@mcmaster.ca>
2024-09-05 21:48:15 -0400

    Add closer callback to add playlist dialog

--- app/src/main/java/com/aaa/vibesmusic/ui/playlists/PlaylistsScreen.kt
+++ app/src/main/java/com/aaa/vibesmusic/ui/playlists/PlaylistsScreen.kt
@@ -14,8 +14,10 @@ import androidx.compose.material3.SnackbarHostState
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.MutableState
+import androidx.compose.runtime.getValue
 import androidx.compose.runtime.mutableStateOf
 import androidx.compose.runtime.remember
+import androidx.compose.runtime.setValue
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.graphics.Color
@@ -46,12 +48,12 @@ fun PlaylistsScreen(
 ) {
     val playlistsScreenViewModel: PlaylistsScreenViewModel = viewModel(factory = PlaylistsScreenViewModel.FACTORY)
 
-    val addPlaylistDialogState: MutableState<Boolean> = remember { mutableStateOf(false) }
+    var addPlaylistDialogState: Boolean by remember { mutableStateOf(false) }
 
     when {
-        addPlaylistDialogState.value -> {
+        addPlaylistDialogState -> {
             AddPlaylistDialog(
-                dialogState = addPlaylistDialogState,
+                closer = { addPlaylistDialogState = false },
                 snackBarHostState = snackBarState,
                 snackBarScope = snackBarScope
             )
@@ -112,7 +114,7 @@ fun PlaylistsScreen(
             }
 
             FloatingActionButton(
-                onClick = { addPlaylistDialogState.value = true },
+                onClick = { addPlaylistDialogState = true },
                 containerColor = colorResource(id = R.color.blue_selected),
                 shape = CircleShape,
                 contentColor = Color.White,
