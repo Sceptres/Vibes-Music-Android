24198a0b3b452a801fbb6a51cf215bc40938ff53
Abdallah Alqashqish <alqashqa@mcmaster.ca>
2024-09-05 21:58:22 -0400

    Change import songs screen package name to songimport

--- app/src/main/java/com/aaa/vibesmusic/ui/songimport/ImportSongsScreen.kt
+++ app/src/main/java/com/aaa/vibesmusic/ui/songimport/ImportSongsScreen.kt
@@ -1,4 +1,4 @@
-package com.aaa.vibesmusic.ui.import
+package com.aaa.vibesmusic.ui.songimport
 
 import androidx.compose.foundation.BorderStroke
 import androidx.compose.foundation.background
@@ -20,7 +20,6 @@ import androidx.compose.ui.res.stringResource
 import androidx.compose.ui.text.font.FontFamily
 import androidx.compose.ui.text.font.FontWeight
 import androidx.compose.ui.text.style.TextAlign
-import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
 import androidx.compose.ui.unit.sp
 import com.aaa.vibesmusic.R
