e5a20ce9d61d9dddc46ac2701be44327d1da579b
Abdallah Alqashqish <alqashqa@mcmaster.ca>
2024-09-06 18:47:31 -0400

    Fix issue with text field scrolling

--- app/src/main/java/com/aaa/vibesmusic/ui/dialogs/common/Fields.kt
+++ app/src/main/java/com/aaa/vibesmusic/ui/dialogs/common/Fields.kt
@@ -1,6 +1,8 @@
 package com.aaa.vibesmusic.ui.dialogs.common
 
 import androidx.compose.foundation.layout.wrapContentSize
+import androidx.compose.foundation.text.KeyboardActions
+import androidx.compose.foundation.text.KeyboardOptions
 import androidx.compose.material3.Text
 import androidx.compose.material3.TextField
 import androidx.compose.material3.TextFieldDefaults
@@ -8,6 +10,8 @@ import androidx.compose.runtime.Composable
 import androidx.compose.runtime.MutableState
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.text.input.ImeAction
+import androidx.compose.ui.text.input.VisualTransformation
 import androidx.compose.ui.unit.sp
 
 @Composable
@@ -35,7 +39,7 @@ fun EditField(valueState: MutableState<String>, placeholderText: String, modifie
                     .wrapContentSize()
             )
         },
-        singleLine = true,
+        maxLines = 1,
         colors = TextFieldDefaults.colors(
             focusedTextColor = Color.White,
             unfocusedTextColor = Color.White,
