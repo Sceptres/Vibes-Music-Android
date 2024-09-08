bf697dd906ea206ac3c7b5a52fc221954179d03c
Abdallah Alqashqish <alqashqa@mcmaster.ca>
2024-09-08 12:11:43 -0400

    Integrate firebase analytics with the app

--- app/src/main/java/com/aaa/vibesmusic/ui/activity/MainActivity.kt
+++ app/src/main/java/com/aaa/vibesmusic/ui/activity/MainActivity.kt
@@ -50,13 +50,20 @@ import com.aaa.vibesmusic.ui.playing.PlayingSongScreen
 import com.aaa.vibesmusic.ui.playlist.PlaylistScreen
 import com.aaa.vibesmusic.ui.playlists.PlaylistsScreen
 import com.aaa.vibesmusic.ui.songimport.ImportSongsScreen
+import com.google.firebase.analytics.FirebaseAnalytics
+import com.google.firebase.analytics.ktx.analytics
+import com.google.firebase.ktx.Firebase
 import kotlinx.coroutines.CoroutineScope
 import kotlinx.coroutines.Job
 
 class MainActivity : AppCompatActivity() {
+    private lateinit var firebaseAnalytics: FirebaseAnalytics
+
     override fun onCreate(savedInstanceState: Bundle?) {
         installSplashScreen()
         super.onCreate(savedInstanceState)
+
+        this.firebaseAnalytics = Firebase.analytics
 
         VibesMusicDatabase.getInstance(applicationContext)
         StorageUtil.setup(this.applicationContext)
