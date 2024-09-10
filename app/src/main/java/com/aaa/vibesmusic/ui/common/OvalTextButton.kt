4533e849c2904a0fbf6ffe362297225110a04f6c
Abdallah Alqashqish <alqashqa@mcmaster.ca>
2024-09-10 19:53:42 -0400

    Extract oval buttons into composable

---
+++ app/src/main/java/com/aaa/vibesmusic/ui/common/OvalTextButton.kt
@@ -0,0 +1,28 @@
+package com.aaa.vibesmusic.ui.common
+
+import androidx.compose.foundation.BorderStroke
+import androidx.compose.material3.Text
+import androidx.compose.material3.TextButton
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.unit.dp
+
+@Composable
+fun OvalTextButton(
+    text: String,
+    color: Color,
+    onClick: () -> Unit,
+    modifier: Modifier = Modifier
+) {
+    TextButton(
+        onClick = onClick,
+        border = BorderStroke(3.dp, color),
+        modifier = modifier
+    ) {
+        Text(
+            text = text,
+            color = color
+        )
+    }
+}
