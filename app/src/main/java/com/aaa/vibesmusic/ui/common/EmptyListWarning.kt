b0899ce26f3fb197469490f37826b6898c5d21a7
Abdallah Alqashqish <alqashqa@mcmaster.ca>
2024-09-15 12:38:40 -0400

    Add text styles for empty list warning

--- app/src/main/java/com/aaa/vibesmusic/ui/common/EmptyListWarning.kt
+++ app/src/main/java/com/aaa/vibesmusic/ui/common/EmptyListWarning.kt
@@ -9,6 +9,7 @@ import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.height
 import androidx.compose.foundation.layout.size
 import androidx.compose.material3.Icon
+import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.remember
@@ -57,7 +58,7 @@ fun EmptyListWarning(
             Text(
                 text = title,
                 color = Color.Gray,
-                fontSize = 20.sp
+                style = MaterialTheme.typography.labelMedium
             )
 
             Spacer(modifier = Modifier.height(5.dp))
@@ -65,7 +66,7 @@ fun EmptyListWarning(
             Text(
                 text = description,
                 color = Color.Gray,
-                fontSize = 15.sp,
+                style = MaterialTheme.typography.labelSmall,
                 textAlign = TextAlign.Center,
                 modifier = Modifier.fillMaxWidth()
             )
