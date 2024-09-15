db784fc40deb8958e95f37b8a363db429bb6a311
Abdallah Alqashqish <alqashqa@mcmaster.ca>
2024-09-15 10:39:11 -0400

    Clean imports across project

--- app/src/main/java/com/aaa/vibesmusic/ui/screens/playlistselect/AddSongToPlaylistsScreen.kt
+++ app/src/main/java/com/aaa/vibesmusic/ui/screens/playlistselect/AddSongToPlaylistsScreen.kt
@@ -17,10 +17,7 @@ import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.res.stringResource
-import androidx.compose.ui.text.font.FontFamily
-import androidx.compose.ui.text.font.FontWeight
 import androidx.compose.ui.unit.dp
-import androidx.compose.ui.unit.sp
 import androidx.lifecycle.viewmodel.compose.viewModel
 import com.aaa.vibesmusic.R
 import com.aaa.vibesmusic.ui.UIUtil
