314cabdaba41ce795a16dd0625ab3ddfbd22f14d
Abdallah Alqashqish <alqashqa@mcmaster.ca>
2024-09-20 23:32:01 -0400

    Cleanup common favourite dropdown menu item

---
+++ app/src/main/java/com/aaa/vibesmusic/ui/dropdown/DropdownItems.kt
@@ -0,0 +1,53 @@
+package com.aaa.vibesmusic.ui.dropdown
+
+import android.content.Context
+import androidx.compose.material3.SnackbarHostState
+import androidx.compose.runtime.Composable
+import androidx.compose.runtime.getValue
+import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.remember
+import androidx.compose.ui.platform.LocalContext
+import com.aaa.vibesmusic.database.data.music.Song
+import com.aaa.vibesmusic.ui.UIUtil
+import com.aaa.vibesmusic.ui.state.FavouriteSongState
+import kotlinx.coroutines.CoroutineScope
+
+@Composable
+fun FavouriteDropdownMenuItem(
+    song: Song,
+    dropdownCloser: () -> Unit,
+    snackBarState: SnackbarHostState,
+    snackBarScope: CoroutineScope
+) {
+    val context: Context = LocalContext.current
+    val state: FavouriteSongState by remember { mutableStateOf(FavouriteSongState(context)) }
+
+    CustomDropdownMenuItem(
+        text = if(!song.isFavourite) "Add to Favourites" else "Remove from Favourites",
+        onClick = {
+            state.toggleSongFavourite(
+                song = song,
+                onSuccess = {
+                    val songAction: String = if(!song.isFavourite) "Added" else "Removed"
+                    val songToFrom: String = if(!song.isFavourite) "to" else "from"
+                    UIUtil.showSnackBar(
+                        snackBarScope = snackBarScope,
+                        snackBarState = snackBarState,
+                        message = "$songAction ${song.name} $songToFrom favourite songs."
+                    )
+                    dropdownCloser()
+                },
+                onFail = {
+                    val songAction: String = if(!song.isFavourite) "add" else "remove"
+                    val songToFrom: String = if(!song.isFavourite) "to" else "from"
+                    UIUtil.showSnackBar(
+                        snackBarScope = snackBarScope,
+                        snackBarState = snackBarState,
+                        message = "Failed to $songAction ${song.name} $songToFrom favourite songs."
+                    )
+                    dropdownCloser()
+                }
+            )
+        }
+    )
+}
--- app/src/main/java/com/aaa/vibesmusic/ui/screens/musiclibrary/composables/MusicLibrarySongDropdown.kt
+++ app/src/main/java/com/aaa/vibesmusic/ui/screens/musiclibrary/composables/MusicLibrarySongDropdown.kt
@@ -1,6 +1,5 @@
 package com.aaa.vibesmusic.ui.screens.musiclibrary.composables
 
-import android.content.Context
 import androidx.compose.material3.SnackbarHostState
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.getValue
@@ -8,16 +7,14 @@ import androidx.compose.runtime.mutableStateOf
 import androidx.compose.runtime.remember
 import androidx.compose.runtime.setValue
 import androidx.compose.ui.Modifier
-import androidx.compose.ui.platform.LocalContext
 import androidx.navigation.NavController
 import com.aaa.vibesmusic.database.data.music.Song
-import com.aaa.vibesmusic.ui.UIUtil
-import com.aaa.vibesmusic.ui.common.CustomDropdown
-import com.aaa.vibesmusic.ui.common.CustomDropdownMenuItem
 import com.aaa.vibesmusic.ui.dialogs.song.delete.DeleteSongDialog
 import com.aaa.vibesmusic.ui.dialogs.song.edit.EditSongDialog
+import com.aaa.vibesmusic.ui.dropdown.CustomDropdown
+import com.aaa.vibesmusic.ui.dropdown.CustomDropdownMenuItem
+import com.aaa.vibesmusic.ui.dropdown.FavouriteDropdownMenuItem
 import com.aaa.vibesmusic.ui.nav.navigateToAddSongToPlaylistScreen
-import com.aaa.vibesmusic.ui.state.FavouriteSongState
 import kotlinx.coroutines.CoroutineScope
 
 @Composable
@@ -30,10 +27,6 @@ fun MusicLibrarySongDropdown(
     snackBarScope: CoroutineScope,
     modifier: Modifier = Modifier
 ) {
-    val context: Context = LocalContext.current
-    val state: FavouriteSongState by remember {
-        mutableStateOf(FavouriteSongState(context))
-    }
     var editDialogState: Boolean by remember { mutableStateOf(false) }
     var deleteDialogState: Boolean by remember { mutableStateOf(false) }
 
@@ -69,33 +62,11 @@ fun MusicLibrarySongDropdown(
             }
         )
 
-        CustomDropdownMenuItem(
-            text = if(!song.isFavourite) "Add to Favourites" else "Remove from Favourites",
-            onClick = {
-                state.toggleSongFavourite(
-                    song = song,
-                    onSuccess = {
-                        val songAction: String = if(!song.isFavourite) "Added" else "Removed"
-                        val songToFrom: String = if(!song.isFavourite) "to" else "from"
-                        UIUtil.showSnackBar(
-                            snackBarScope = snackBarScope,
-                            snackBarState = snackBarState,
-                            message = "$songAction ${song.name} $songToFrom favourite songs."
-                        )
-                        closer()
-                    },
-                    onFail = {
-                        val songAction: String = if(!song.isFavourite) "add" else "remove"
-                        val songToFrom: String = if(!song.isFavourite) "to" else "from"
-                        UIUtil.showSnackBar(
-                            snackBarScope = snackBarScope,
-                            snackBarState = snackBarState,
-                            message = "Failed to $songAction ${song.name} $songToFrom favourite songs."
-                        )
-                        closer()
-                    }
-                )
-            }
+        FavouriteDropdownMenuItem(
+            song = song,
+            dropdownCloser = closer,
+            snackBarState = snackBarState,
+            snackBarScope = snackBarScope
         )
 
         CustomDropdownMenuItem(
